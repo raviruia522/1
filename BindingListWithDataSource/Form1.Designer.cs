9ac8a40272eeaea20b42e0bc6daae5a35d8d81fb
zNew8 <44082224+raviruia522@users.noreply.github.com>
2025-08-31 17:47:21 +0700

    Add files via upload

---
+++ BindingListWithDataSource/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BindingListWithDataSource
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            btnAdd = new Button();
+            btnRemove = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(58, 38);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(660, 331);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(68, 3);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new Point(182, 3);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(94, 29);
+            btnRemove.TabIndex = 2;
+            btnRemove.Text = "Remove";
+            btnRemove.UseVisualStyleBackColor = true;
+            btnRemove.Click += btnRemove_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRemove);
+            Controls.Add(btnAdd);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnAdd;
+        private Button btnRemove;
+    }
+}
---
+++ BindingListWithDataSource/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace BindingListWithDataSource
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<Person> people = new BindingList<Person>();
+        private BindingSource bindingSource = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Gắn BindingList vào BindingSource
+            bindingSource.DataSource = people;
+
+            // Gắn BindingSource vào DataGridView
+            dataGridView1.DataSource = bindingSource;
+
+            // Thêm vài mẫu
+            people.Add(new Person { Id = 1, Name = "Nguyen Van A", Age = 25 });
+            people.Add(new Person { Id = 2, Name = "Tran Thi B", Age = 30 });
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Thêm mới thông qua people
+            people.Add(new Person
+            {
+                Id = people.Count + 1,
+                Name = "User " + (people.Count + 1),
+                Age = 20 + people.Count
+            });
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (bindingSource.Current is Person person)
+            {
+                // Xóa bằng BindingSource hoặc trực tiếp từ people
+                bindingSource.Remove(person);
+                // people.Remove(person); // cũng được
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+    }
+}
